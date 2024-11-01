4968fcac9548668699f306e2eaf300b7f14b5d0b
paczkico <admin@paczki.co>
2024-11-01 18:50:36 -0400

    Delete lib/discourse_user_feedbacks directory

--- lib/discourse_user_feedbacks/user_extension.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module DiscourseUserFeedbacks::UserExtension
-  def self.prepended(base)
-    base.has_many :feedbacks, class_name: 'DiscourseUserFeedbacks::UserFeedback'
-  end
-end
